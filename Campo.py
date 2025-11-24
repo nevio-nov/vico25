69e42949575dfbef022fb6b61785ccb3128ad8fc
nevio-nov <nevio.noviello@gmail.com>
2025-11-24 12:49:32 +0100

    completa le funzioni

--- Campo.py
+++ Campo.py
@@ -1,14 +1,18 @@
 import random
-
+import time
 
 class Campo:
     def __init__(self, n, navi):
         self.navi = navi
         self.matr = []
+        self.ematr = []
+        self.cnavi = []
         for i in range(n):
             self.matr.append([])
+            self.ematr.append([])
             for j in range(n):
                 self.matr[i].append('0')
+                self.ematr[i].append('0')
 
     def popola_rand(self):
         for nave in self.navi:
@@ -18,8 +22,8 @@ class Campo:
             z = ''
             while not allEmpty:
                 allEmpty = True
-                x = random.randint(0, 9)
-                y = random.randint(0, 9)
+                x = random.randint(0, len(self.matr) - 1)
+                y = random.randint(0, len(self.matr) - 1)
                 z = random.choice(["down", "right"])
                 if z == "down":
                     if self.can_go_down(x, y, nave):
@@ -39,24 +43,6 @@ class Campo:
                         allEmpty = False
             self.place(x, y, z, nave)
 
-    def popola_rand_down(self):
-        for nave in self.navi:
-            allEmpty = False
-            x = 0
-            y = 0
-            while not allEmpty:
-                allEmpty = True
-                x = random.randint(0, 9)
-                y = random.randint(0, 9)
-                if y < len(self.matr) - nave:
-                    for i in range(nave):
-                        if self.matr[y + i][x] == 'X':
-                            allEmpty = False
-                else:
-                    allEmpty = False
-            for i in range(nave):
-                self.matr[y + i][x] = 'X'
-
     def printcampo(self):
         for i in range(len(self.matr)):
             s = ''
@@ -65,10 +51,68 @@ class Campo:
             print(s)
 
     def can_go_down(self, x, y, nave):
-
+        if y + nave <= len(self.matr):
+            for i in range(nave):
+                if self.matr[y + i][x] == 'X':
+                    return False
+        else:
+            return False
+        return True
 
     def can_go_right(self, x, y, nave):
-
+        if x + nave <= len(self.matr):
+            for i in range(nave):
+                if self.matr[y][x + i] == 'X':
+                    return False
+        else:
+            return False
+        return True
 
     def place(self, x, y, z, nave):
+        coordinate = []
+        if z == "down":
+            for i in range(nave):
+                self.matr[y + i][x] = 'X'
+                coordinate.append((y + i, x))
+        else:
+            for i in range(nave):
+                self.matr[y][x + i] = 'X'
+                coordinate.append((y, x + i))
+        self.cnavi.append(coordinate)
 
+
+    def printecampo(self):
+        s = '   '
+        for i in range(len(self.ematr)):
+            s += ' ' + str(i) + ' '
+        print(s)
+        print('  ' + '-'*(len(s)-3))
+        for i in range(len(self.ematr)):
+            s = str(i) + ' |'
+            for j in range(len(self.ematr)):
+                s += ' ' + str(self.ematr[i][j]) + ' '
+            print(s)
+
+def playerturn(campi):
+    print("write player :)")
+    time.sleep(1)
+
+
+def botturn(campi):
+    print("write bot :)")
+    time.sleep(1)
+
+
+def vs_bot(n, navi):
+    player = random.randint(0, 1) #0 player, 1 bot
+    campi = [Campo(n, navi), Campo(n, navi)]
+    campi[0].popola_rand()
+    campi[1].popola_rand()
+
+    while True:
+        print('turn of player', player)
+        if player == 0:
+            playerturn(campi)
+        else:
+            botturn(campi)
+        player = (player + 1) % 2
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import Campo as C
 
 
 if __name__ == "__main__":
-    c = C.Campo(10, [4, 3, 2])
-    c.popola_rand()
-    c.printcampo()
-
-
+    n = 5
+    navi = [4, 3, 2, 1]
+    C.vs_bot(n, navi)
